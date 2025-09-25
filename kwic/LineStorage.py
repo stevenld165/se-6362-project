9fbf7d0dfc66a1618991f1dc5e7df06045dd76b0
Steven <stevenld165@gmail.com>
2025-09-25 16:15:36 -0500

    Added core functionality of KWIC system, and basic frontend

--- kwic/LineStorage.py
+++ kwic/LineStorage.py
@@ -1,3 +1,16 @@
-# I think this should be a class where it has a method where 
-# you take in a string "The Lion King$Beauty and the Beast" and turn it into 
-# a list of split up lines as arrays of words
+# Takes strings and splits them by newline characters
+# Stores these in its storage array
+# Stores them as [[word, word2], [word, word2, word3]] pre-split
+class LineStorage:
+    def __init__(self):
+        self.lines = []
+    def set_lines(self, text):
+        self.lines = [line.strip().split() for line in text.splitlines() if line.strip()]
+    def get_line(self, index):
+        return self.lines[index]
+    def get_all(self):
+        return [" ".join(line) for line in self.lines]
+    def count(self):
+        return len(self.lines)
+    def reset(self):
+        self.lines = []
